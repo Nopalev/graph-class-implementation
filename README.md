c655c433f467d987a248f29964906ef3022bf743
Naufal Faadhilah <faadhilah56@gmail.com>
2022-05-11 09:07:14 +0700

    adding a little bit explanation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# graph-class-implementation
+# Graph Class Implementation
 C++ implementation of graph
+
+Based on [Alpro ITS's data structure module](https://github.com/AlproITS/StrukturData)
+
+All transversal function are checked on [visualgo](https://visualgo.net/en/dfsbfs)
+
+Feel free to use!
+
+note: [unweighted_graph.h](https://github.com/Nopalev/graph-class-implementation/blob/main/unweighted_graph.h) isthe main graph class, while [graph_test.cpp](https://github.com/Nopalev/graph-class-implementation/blob/main/graph_test.cpp) tells you how to use it.
--- unweighted_graph.h
+++ unweighted_graph.h
@@ -12,37 +12,45 @@ class unweighted_graph{
     public:
     unweighted_graph(long vertex, bool isDirected);
     void addEdge(long vertex1, long vertex2);
+    long vertexCount();
     long edgeCount();
     void dfs(long vertex, vector<long> &v);
     void bfs(long vertex, vector<long> &v);
 };
 
-unweighted_graph::unweighted_graph(long vertex, bool isDirected){
-    this->vertex = vertex;
-    this->edge = 0;
-    this->isDirected = isDirected;
+unweighted_graph::unweighted_graph(long vertex, bool isDirected){ // this is the class contructor
+    this->vertex = vertex; // initialize how many vertex there will be
+    this->edge = 0; // at first, the graph doesn't have any edge. you may use addEdge
+    this->isDirected = isDirected; // to determine the graph is directed or not (directed graph is under development, may unstable)
     for(long i = 0; i < vertex; i++){
-        adjList.push_back({});
+        adjList.push_back({}); // adding the vertex into the adjacency list
     }
 }
 
-void unweighted_graph::addEdge(long vertex1, long vertex2){
-    adjList[vertex1].push_back(vertex2);
-    if(!isDirected) adjList[vertex2].push_back(vertex1);
+void unweighted_graph::addEdge(long vertex1, long vertex2){ // a function to add aditional edge
+    if(find(adjList[vertex1].begin(), adjList[vertex1].end(), vertex2) != adjList[vertex1].end()) return; // just to make sure the edge is not present already
+    adjList[vertex1].push_back(vertex2); // adding the edge
+    if(!isDirected) adjList[vertex2].push_back(vertex1); // adding additional edge if the graph is undirected
     edge++;
 }
 
-long unweighted_graph::edgeCount(){
-    return edge;
+long unweighted_graph::vertexCount(){ // a function that tells you how many vertex(s) inside this graph
+    return vertex; // may come handy when debugging
 }
 
-void unweighted_graph::dfs(long vertex, vector<long> &v){
-    vector<bool> visited(this->vertex, false);
+long unweighted_graph::edgeCount(){ // a function that tells you how many edge(s) inside this graph
+    return edge; // also may come handy when debugging
+}
+
+void unweighted_graph::dfs(long vertex, vector<long> &v){ //function call for dfs transversal
+    // you need a vertex as a starting point and a vector passed into the function
+    // the vector will store transversal result
+    vector<bool> visited(this->vertex, false); // a vector that marks all visited vertex
     v.clear();
     depth_first_search(vertex, visited, v);
 }
 
-void unweighted_graph::depth_first_search(long vertex, vector<bool> &visited, vector<long> &v){
+void unweighted_graph::depth_first_search(long vertex, vector<bool> &visited, vector<long> &v){ //utility function for dfs transversal
     visited[vertex] = true;
 
     v.push_back(vertex);
@@ -52,13 +60,15 @@ void unweighted_graph::depth_first_search(long vertex, vector<bool> &visited, ve
     }
 }
 
-void unweighted_graph::bfs(long vertex, vector<long> &v){
-    vector<bool> visited(this->vertex, false);
+void unweighted_graph::bfs(long vertex, vector<long> &v){ //function call for bfs transversal
+    // you need a vertex as a starting point and a vector passed into the function
+    // the vector will store transversal result
+    vector<bool> visited(this->vertex, false); // a vector that marks all visited vertex
     v.clear();
     breadth_first_search(vertex, visited, v);
 }
 
-void unweighted_graph::breadth_first_search(long vertex, vector<bool> &visited, vector<long> &v){
+void unweighted_graph::breadth_first_search(long vertex, vector<bool> &visited, vector<long> &v){ // utility function for bfs transversal
     queue<long> q;
     q.push(vertex);
     visited[vertex] = true;
